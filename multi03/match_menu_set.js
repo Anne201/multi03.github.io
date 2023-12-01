8d1142b06da906389804e38a34a6c77f786595ee
Anne201 <118590269+Anne201@users.noreply.github.com>
2023-12-01 19:45:53 +0800

    v1

---
+++ multi03/match_menu_set.js
@@ -0,0 +1,9 @@
+menu_title='10以內減法 對對碰選單';
+menu_items = new Array(
+  '10的減法-1,match_set_1.js'
+, '9的減法-2,match_set_2.js'
+, '8的減法-3,match_set_3.js'
+, '7的減法-4,match_set_4.js'
+, '5/6的減法-5&6,match_set_5.js'
+, '4/3/2/1的加法-7&8&9,match_set_6.js'
+);
